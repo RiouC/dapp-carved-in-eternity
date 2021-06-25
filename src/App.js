765eb98a645926e8733fe31a48e924b6a30ad623
RiouC <riou.christophe@protonmail.com>
2021-06-25 03:01:08 +0200

    [App] very basic layout of the dapp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Input } from "@chakra-ui/react";
+import { VStack, Textarea, Button } from "@chakra-ui/react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="App">
       <h1>Carved in Eternity</h1>
-      <Input />
-      {/* <TextArea /> */}
+      <VStack>
+        <Textarea />
+        <Button>Submit</Button>
+      </VStack>
     </div>
   );
 }
