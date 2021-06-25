38927ff4b25dc8d256f92564def6f3b1f28a2b3b
RiouC <riou.christophe@protonmail.com>
2021-06-25 02:56:43 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Input } from "@chakra-ui/react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carved in Eternity</h1>
+      <Input />
+      {/* <TextArea /> */}
+    </div>
+  );
+}
+
+export default App;
